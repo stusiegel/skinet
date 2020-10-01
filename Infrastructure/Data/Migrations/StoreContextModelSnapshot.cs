1d6279a886fde0419c0bf1c45abebd7348d46081
Stuart Siegel <Stuart_Siegel@uhaul.com>
2020-09-30 23:47:56 -0700

    Initial Commit

---
+++ Infrastructure/Data/Migrations/StoreContextModelSnapshot.cs
@@ -0,0 +1,37 @@
+﻿ 
+using Core.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
+
+namespace Infrastructure.Data.Migrations
+{
+    [DbContext(typeof(StoreContext))]
+    partial class StoreContextModelSnapshot : ModelSnapshot
+    {
+        protected override void BuildModel(ModelBuilder modelBuilder)
+        {
+
+            modelBuilder
+                .HasAnnotation("ProductVersion", "3.1.8");
+
+            modelBuilder.Entity("API.Entities.Product", b =>
+                {
+                    b.Property<int>("Id")
+                        .ValueGeneratedOnAdd()
+                        .HasColumnType("INTEGER");
+
+                    b.Property<string>("Name")
+                        .HasColumnType("TEXT");
+
+                    b.HasKey("Id");
+
+                    b.ToTable("Products");
+                });
+        }
+
+    }
+}
+
+
+ 
